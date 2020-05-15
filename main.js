71cd5d0428e27445265af9c3c6572b457fad08a4
Donovan Davis <dt-davis@Hotmail.com>
2020-05-15 16:21:35 -0500

    added javascript- show all rolls added html -show all rolls finished assessment according to rubric todo css, extra credit

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const rollButton = document.querySelector('.rollButton')
+const rollButton = document.querySelector('.roll!Button')
 const userRollamount = document.querySelector('.userRolls')
 const total = document.querySelector('.total')
+const allRollbutton = document.querySelector('.showAllrolls')
+const allRolldisplay = document.querySelector('.allRolls')
 let dieRolls = []
 let dieTotal = 0
 let rollAmount = 0
+let allRollAmnt = 0
 
 
 rollButton.addEventListener('click' , function(){
@@ -13,13 +16,27 @@ rollButton.addEventListener('click' , function(){
     console.log(rollAmount)
     let counter = 0
     while (counter < rollAmount){
-        dieRolls[counter] = Math.round((Math.random() * 5) + 1)
+        dieRolls.push(Math.round((Math.random() * 5) + 1))
         dieTotal += dieRolls[counter]
         counter ++
     }
     
     console.log(dieRolls)
     total.innerHTML = dieTotal
+})
+
+allRollbutton.addEventListener('click' , function(){
+    let counter = 0
+    console.log(dieRolls.length)
+    allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set:' + (allRollAmnt + 1).toString() + '</ul>'
+    while ( counter < dieRolls.length){
+        let rollList = document.querySelector('.newSet' + allRollAmnt.toString())
+        rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
+        console.log(rollList)
+        counter ++
+    }
+    allRollAmnt++
+    console.log(allRolldisplay)
 })
 
 console.log(dieRolls)
