73a2f6aebf2959b89467b8ad0190b119254e1817
Donovan Davis <dt-davis@Hotmail.com>
2020-05-15 16:58:43 -0500

    added a side input

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const total = document.querySelector('.total')
 const allRollbutton = document.querySelector('.showAllrolls')
 const allRolldisplay = document.querySelector('.allRolls')
 const restButton = document.querySelector('.restButton')
+const userSides =  document.querySelector('.howMany')
+userSides.value = 6
 let dieRolls = []
 let dieTotal = 0
 let rollAmount = 0
@@ -17,19 +19,19 @@ rollButton.addEventListener('click' , function(){
     console.log(rollAmount)
     let counter = 0
     while (counter < rollAmount){
-        dieRolls.push(Math.round((Math.random() * 5) + 1))
+        dieRolls.push(Math.round((Math.random() * (userSides.value - 1)) + 1))
         dieTotal += dieRolls[counter]
         counter ++
     }
     
     console.log(dieRolls)
-    total.innerHTML = 'Total of Rolls:' + dieTotal
+    total.innerHTML = 'Total of Rolls of the ' + userSides.value + ' sided dice:' + dieTotal
 })
 
 allRollbutton.addEventListener('click' , function(){
     let counter = 0
     console.log(dieRolls.length)
-    allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set:' + (allRollAmnt + 1).toString() + '</ul>'
+    allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set (sides:' + userSides.value + '):' + (allRollAmnt + 1).toString() + '</ul>'
     while ( counter < dieRolls.length){
         let rollList = document.querySelector('.newSet' + allRollAmnt.toString())
         rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
@@ -46,5 +48,6 @@ restButton.addEventListener('click' , function(){
     allRollAmnt = 0
     total.innerHTML = 'Total of Rolls:--'
     userRollamount.value = ''
+    userSides.value = 6
 })
 console.log(dieRolls)
