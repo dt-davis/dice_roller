40f6037e84d897e1969c3a1d478eeb9aa95e94ee
Donovan Davis <dt-davis@Hotmail.com>
2020-05-15 22:44:20 -0500

    started css styling added panel adding and changing - expand button for popup screen todo - css, find solution for rolllist counter possible problem

--- main.js
+++ main.js
@@ -2,47 +2,58 @@ const rollButton = document.querySelector('.rollButton')
 const userRollamount = document.querySelector('.userRolls')
 const total = document.querySelector('.total')
 const allRollbutton = document.querySelector('.showAllrolls')
-const allRolldisplay = document.querySelector('.allRolls')
+let allRolldisplay = document.querySelector('.leftPanels')
 const restButton = document.querySelector('.restButton')
-const userSides =  document.querySelector('.howMany')
+const userSides = document.querySelector('.howMany')
 userSides.value = 6
 let dieRolls = []
 let dieTotal = 0
 let rollAmount = 0
 let allRollAmnt = 0
+let panelChange = 6
 
 
-rollButton.addEventListener('click' , function(){
+rollButton.addEventListener('click', function () {
     dieRolls = []
     dieTotal = 0
     rollAmount = userRollamount.value
     console.log(rollAmount)
     let counter = 0
-    while (counter < rollAmount){
+    while (counter < rollAmount) {
         dieRolls.push(Math.round((Math.random() * (userSides.value - 1)) + 1))
         dieTotal += dieRolls[counter]
-        counter ++
+        counter++
     }
-    
+
     console.log(dieRolls)
     total.innerHTML = 'Total of Rolls of the ' + userSides.value + ' sided dice:' + dieTotal
 })
 
-allRollbutton.addEventListener('click' , function(){
+allRollbutton.addEventListener('click', function () {
     let counter = 0
+    let limiter = 5
     console.log(dieRolls.length)
-    allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
-    while ( counter < dieRolls.length){
+    if (allRollAmnt < panelChange) {
+        allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
+    } else if (allRollAmnt < (panelChange * 2)) {
+        allRolldisplay = document.querySelector('.rightPanels')
+        allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
+    }
+    while (counter < dieRolls.length) {
         let rollList = document.querySelector('.newSet' + allRollAmnt.toString())
-        rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
+        if (counter < limiter) {
+            rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
+        } else if (counter === limiter) {
+            rollList.innerHTML += "<li><button class=\"panelExpand\">...</button></li>"
+        }
         console.log(rollList)
-        counter ++
+        counter++
     }
     allRollAmnt++
     console.log(allRolldisplay)
 })
 
-restButton.addEventListener('click' , function(){
+restButton.addEventListener('click', function () {
     allRolldisplay.innerHTML = ''
     dieRolls = []
     allRollAmnt = 0
