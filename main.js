586b2a95e03f42efd28011ebcdc1b99e918e099a
Donovan Davis <dt-davis@Hotmail.com>
2020-05-15 23:26:14 -0500

    fixed reset button add if/else for valid roll set

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const rollButton = document.querySelector('.rollButton')
 const userRollamount = document.querySelector('.userRolls')
 const total = document.querySelector('.total')
-const allRollbutton = document.querySelector('.showAllrolls')
+const recordButton = document.querySelector('.showAllrolls')
 let allRolldisplay = document.querySelector('.leftPanels')
 const restButton = document.querySelector('.restButton')
 const userSides = document.querySelector('.howMany')
@@ -29,32 +29,39 @@ rollButton.addEventListener('click', function () {
     total.innerHTML = 'Total of Rolls of the ' + userSides.value + ' sided dice:' + dieTotal
 })
 
-allRollbutton.addEventListener('click', function () {
+recordButton.addEventListener('click', function () {
     let counter = 0
     let limiter = 5
     console.log(dieRolls.length)
-    if (allRollAmnt < panelChange) {
-        allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
-    } else if (allRollAmnt < (panelChange * 2)) {
-        allRolldisplay = document.querySelector('.rightPanels')
-        allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
-    }
-    while (counter < dieRolls.length) {
-        let rollList = document.querySelector('.newSet' + allRollAmnt.toString())
-        if (counter < limiter) {
-            rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
-        } else if (counter === limiter) {
-            rollList.innerHTML += "<li><button class=\"panelExpand\">...</button></li>"
+    if (dieRolls.length > 0) {
+        if (allRollAmnt < panelChange) {
+            allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
+        } else if (allRollAmnt < (panelChange * 2)) {
+            allRolldisplay = document.querySelector('.rightPanels')
+            allRolldisplay.innerHTML += '<ul class=\'newSet' + allRollAmnt.toString() + '\'>Recorded Set ' + (allRollAmnt + 1).toString() + '(sides:' + userSides.value + '): </ul>'
         }
-        console.log(rollList)
-        counter++
+        while (counter < dieRolls.length) {
+            let rollList = document.querySelector('.newSet' + allRollAmnt.toString())
+            if (counter < limiter) {
+                rollList.innerHTML += "<li>" + dieRolls[counter] + "</li>"
+            } else if (counter === limiter) {
+                rollList.innerHTML += "<li><button class=\"panelExpand\">...</button></li>"
+            }
+            console.log(rollList)
+            counter++
+        }
+        allRollAmnt++
+    }else {
+        alert('Please Roll A Valid Set')
     }
-    allRollAmnt++
+    
     console.log(allRolldisplay)
 })
 
 restButton.addEventListener('click', function () {
-    allRolldisplay.innerHTML = ''
+    document.querySelector('.leftPanels').innerHTML = ''
+    document.querySelector('.rightPanels').innerHTML = ''
+    allRolldisplay = document.querySelector('.leftPanels')
     dieRolls = []
     allRollAmnt = 0
     total.innerHTML = 'Total of Rolls:--'
